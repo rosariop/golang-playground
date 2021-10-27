22a8635a642c244142796108710c91d15f613005
Rosario <rosario.polito@gmx.de>
2021-10-27 20:02:08 +0200

    step: adding struct

---
+++ src/Content.go
@@ -0,0 +1,5 @@
+package main
+
+type Content struct {
+	Welcoming string
+}
--- src/main.go
+++ src/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
-)
 
-type Content struct {
-	Welcoming string
-}
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func renderWelcome(responseWriter http.ResponseWriter, request *http.Request) {
 	htmlTemplate := template.Must(template.ParseFiles("template/welcome.html"))
@@ -17,12 +16,22 @@ func renderWelcome(responseWriter http.ResponseWriter, request *http.Request) {
 
 func main() {
 
+	//Database
+	fmt.Printf("Starting Database connection! \n")
+
+	// Create database handle and check if driver is present
+	db, _ := sql.Open("mysql", "127.0.0.1:3306")
+	defer db.Close()
+
+	//Connect to db and check version
+	version := ""
+	db.QueryRow("SELECT VERSION()").Scan(&version)
+	fmt.Printf("Connected to: %s", version)
+
+	//Webserver
 	port := "8080"
-
 	http.HandleFunc("/", renderWelcome)
-
 	fmt.Println("Hello")
-
 	fmt.Printf("Running on Port %s \n", port)
 	fmt.Printf("http://localhost:%s \n", port)
 	http.ListenAndServe(":"+port, nil)
